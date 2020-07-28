4b79fb8c0d2eea5cf82256b1872c4fcab0f5e4cf
Marcio Alves de Oliveira <64289932+madeoliveira@users.noreply.github.com>
2020-07-28 19:01:38 -0300

    Overlay, super, @Override

--- Bank/src/application/Program.java
+++ Bank/src/application/Program.java
@@ -8,14 +8,16 @@ public class Program {
 
 	public static void main(String[] args) {
 
-		Account acc = new Account(1001, "Alex", 0.0);
+		Account acc = new Account(1001, "Alex", 1000.0);
 		BusinessAccount bacc = new BusinessAccount(1002, "Maria", 0.0, 500.0);
+		
 		
 		// UPCASTING
 		
 		Account acc1 = bacc;
-		Account acc2 = new BusinessAccount(1003, "Bob", 0.0, 200.0);
-		Account acc3 = new SavingsAccount(1004, "Anna", 0.0, 0.01);
+		Account acc2 = new BusinessAccount(1003, "Bob", 1000.0, 500.0);
+		Account acc3 = new SavingsAccount(1004, "Anna", 1000.0, 0.01);
+		
 		
 		// DOWNCASTING
 		
@@ -34,5 +36,13 @@ public class Program {
 			acc5.updateBalance();
 			System.out.println("Update!");
 		}
+		
+		acc.withdraw(200.0);
+		acc2.withdraw(200.0);
+		acc3.withdraw(200.0);
+		
+		System.out.println(acc.getBalance());
+		System.out.println(acc3.getBalance());
+		System.out.println(acc2.getBalance());
 	}
 }
