83e791c2d73f6ace8bead8cd8345dd16354472b3
Marcio Alves de Oliveira <64289932+madeoliveira@users.noreply.github.com>
2020-07-30 18:06:55 -0300

    Implemented FileWriter and Buffered.

---
+++ TextFile/src/fileWriterBufferedWriter/Program.java
@@ -0,0 +1,27 @@
+package fileWriterBufferedWriter;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Program {
+
+	public static void main(String[] args) {
+		String[] lines = new String[] {"Good morning", "Good afternoon","Gool night"};
+		
+		
+		String path = "c:\\temp\\out.txt";
+		
+		try (BufferedWriter bw = new BufferedWriter(new FileWriter(path, true))){
+			
+			for (String line : lines) {
+				bw.write(line);
+				bw.newLine();
+			}
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+}
