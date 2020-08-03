32e458b32400632d7e345f36a135bd224a700e55
Marcio Alves de Oliveira <64289932+madeoliveira@users.noreply.github.com>
2020-08-03 11:20:06 -0300

    Generics will be implemented.

---
+++ GenericExample/src/application/Program.java
@@ -0,0 +1,27 @@
+package application;
+
+import java.util.Scanner;
+
+import services.PrintService;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		PrintService ps = new PrintService();
+		
+		System.out.print("How many value?");
+		int n = sc.nextInt();
+		
+		for(int i =0; i<n; i++) {
+			Integer value = sc.nextInt();
+			ps.addValue(value);
+		}
+		ps.print();
+		System.out.println("First: "+ ps.first());
+		sc.close();
+	}
+
+}
