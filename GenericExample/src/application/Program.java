7889f3b84e15db6506855293e5c53c21200cb323
Marcio Alves de Oliveira <64289932+madeoliveira@users.noreply.github.com>
2020-08-03 11:35:59 -0300

    Generic example implemented.

--- GenericExample/src/application/Program.java
+++ GenericExample/src/application/Program.java
@@ -10,17 +10,33 @@ public class Program {
 		
 		Scanner sc = new Scanner(System.in);
 		
-		PrintService ps = new PrintService();
+		PrintService<Integer> ps = new PrintService<>();
+		PrintService<String> pss = new PrintService<>();
 		
-		System.out.print("How many value?");
+		System.out.print("How many value Integer?");
 		int n = sc.nextInt();
+		
 		
 		for(int i =0; i<n; i++) {
 			Integer value = sc.nextInt();
 			ps.addValue(value);
 		}
+		
+		System.out.print("How many value String?");
+		int s = sc.nextInt();
+		for(int i =0; i<s; i++) {
+			String value = sc.next();
+			pss.addValue(value);
+		}
+		
 		ps.print();
+		Integer x=ps.first();
 		System.out.println("First: "+ ps.first());
+		
+		pss.print();
+		String xs=pss.first();
+		System.out.println("First: "+ pss.first());
+		
 		sc.close();
 	}
 
