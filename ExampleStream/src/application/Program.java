242d623efc8d044a576146b9fcecf5f5144a9d91
Marcio Alves de Oliveira <64289932+madeoliveira@users.noreply.github.com>
2020-08-07 09:06:24 -0300

    Example Stream.

--- ExampleStream/src/application/Program.java
+++ ExampleStream/src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Program {
@@ -12,7 +13,17 @@ public class Program {
 
 		Stream<Integer> st1 = list.stream().map(x -> x * 10);
 		System.out.println(Arrays.toString(st1.toArray()));
-
+		
+		int sum = list.stream().reduce(0, (x,y)->x+y);
+		System.out.println("Sum = "+ sum);
+		
+		List<Integer>newList=list.parallelStream()
+				.filter(x -> x % 2 ==0)
+				.map(x-> x * 10)
+				.collect(Collectors.toList());
+		System.out.println(Arrays.deepToString(newList.toArray()));
+		
+		
 		Stream<String> st2 = Stream.of("Maria", "João", "Bob");
 		System.out.println(Arrays.toString(st2.toArray()));
 
