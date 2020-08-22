834fdefa9bf6a02a116b3e6e9fde20610dd9b800
Marcio Alves de Oliveira <64289932+madeoliveira@users.noreply.github.com>
2020-08-22 12:42:25 -0300

    Adding database access.

--- workshop-javafx-jdbc/src/model/entities/Department.java
+++ workshop-javafx-jdbc/src/model/entities/Department.java
@@ -5,24 +5,24 @@ import java.io.Serializable;
 public class Department implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	private Integer Id;
+
+	private Integer id;
 	private String name;
-
+	
 	public Department() {
-
 	}
 
 	public Department(Integer id, String name) {
-		Id = id;
+		this.id = id;
 		this.name = name;
 	}
 
 	public Integer getId() {
-		return Id;
+		return id;
 	}
 
 	public void setId(Integer id) {
-		Id = id;
+		this.id = id;
 	}
 
 	public String getName() {
@@ -37,7 +37,7 @@ public class Department implements Serializable {
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + ((Id == null) ? 0 : Id.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		return result;
 	}
 
@@ -50,17 +50,16 @@ public class Department implements Serializable {
 		if (getClass() != obj.getClass())
 			return false;
 		Department other = (Department) obj;
-		if (Id == null) {
-			if (other.Id != null)
+		if (id == null) {
+			if (other.id != null)
 				return false;
-		} else if (!Id.equals(other.Id))
+		} else if (!id.equals(other.id))
 			return false;
 		return true;
 	}
 
 	@Override
 	public String toString() {
-		return "Department [Id=" + Id + ", name=" + name + "]";
+		return "Department [id=" + id + ", name=" + name + "]";
 	}
-
 }
